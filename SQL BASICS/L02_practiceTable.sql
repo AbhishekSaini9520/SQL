5e5d360fdb1046269651f3944beed4aa37548e1e
Abhishek Saini <abhisheksaini200404@gmail.com>
2025-06-30 18:52:58 +0530

    lecture 2 iscompleted

---
+++ SQL BASICS/L02_practiceTable.sql
@@ -0,0 +1,11 @@
+
+
+select * from EmployeeRecords
+
+select distinct firstname from Employeerecords
+
+select distinct firstname,lastname from employeerecords
+
+select distinct Salary from employeerecords
+
+select distinct * from employeerecords
