e7b5f21742d39e182b8aa6aa02403b8ea2ec751a
Abhishek Saini <abhisheksaini200404@gmail.com>
2025-06-30 18:36:39 +0530

    lecture 1 is completed

---
+++ SQL BASICS/L01_practiceTable.sql
@@ -0,0 +1,15 @@
+
+
+select * from Employees
+
+select FirstName from Employees
+
+select firstname , lastname from Employees
+
+select firstname , Salary from Employees
+
+select EmployeeID, concat(FirstName,' ',LastName) from Employees
+
+select EmployeeID, concat(FirstName,' ',LastName) [Full Name] from Employees
+
+select EmployeeID, concat(FirstName,' ',LastName) as [Full Name] from Employees
